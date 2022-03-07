86a6153204810158dc4a78eb8aaa036f3414f21e
Victor Hernández <victor.hhernandez27@gmail.com>
2022-03-07 15:17:20 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SomaParImpar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0, numeroPar = 0, numeroImpar = 0;
+
+            do
+            {
+                Console.WriteLine("Digite um numero inteiro e positivo: ");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                if (numero < 0)
+                {
+                    Console.WriteLine("Não são permitidos números negativos");
+                }
+                else
+                {
+                    if (numero > 1000)
+                    {
+                        Console.WriteLine("Encerrando o programa");
+                    }
+                    else
+                    {
+                        if (numero % 2 == 0)
+                        {
+                            numeroPar = numeroPar + numero;
+                        }
+                        else
+                        {
+                            numeroImpar = numeroImpar + numero;
+                        }
+                    }
+                }
+
+            } while (numero <= 1000);
+
+            Console.WriteLine("Soma dos numeros pares é {0} e a Soma dos numeros impares é {1}", numeroPar,numeroImpar);
+
+            Console.ReadKey();
+
+        }
+    }
+}
